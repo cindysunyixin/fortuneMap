092fde071c28ba77be2efcbbfc62c6d49d3f1289
koerino <koerino@gmail.com>
2016-09-24 15:13:10 -0400

    modified some files; deleted redundant files

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 // serve static assets
 app.use(express.static(__dirname + '/public'));
 
+// render index page as default home page
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
