79843a780f5e4a1676e0ce3192e3b01bbaa4f346
koerino <koerino@gmail.com>
2016-09-24 19:07:36 -0400

    fixed display

--- public/js/companyViz.js
+++ public/js/companyViz.js
@@ -1,54 +1,56 @@
 function companyViz(jsonData, map) {
-	var overlay = new google.maps.OverlayView();
+    var overlay = new google.maps.OverlayView();
 
-	overlay.onAdd = function() {
-	    var layer = d3.select(this.getPanes().overlayLayer).append("div")
-	        .attr("class", "companies");
+    overlay.onAdd = function () {
+        var layer = d3.select(this.getPanes().overlayLayer).append("div")
+            .attr("class", "companies");
 
-        overlay.draw = function() {
-	      var projection = this.getProjection(),
-	          padding = 10;
+        overlay.draw = function () {
+            var projection = this.getProjection(),
+                padding = 10;
 
-	      var marker = layer.selectAll("svg")
-	          .data(d3.entries(data))
-	          .each(transform) // update existing markers
-	        .enter().append("svg")
-	          .each(transform)
-	          .attr("class", "marker");
+            var marker = layer.selectAll("svg")
+                .data(d3.entries(data))
+                .each(transform) // update existing markers
+                .enter().append("svg")
+                .each(transform)
+                .attr("class", "marker");
 
-	      // Add a circle.
-	      marker.append("circle")
-	          .attr("r", 4.5)
-	          .attr("cx", padding)
-	          .attr("cy", padding);
+            // Add a circle.
+            marker.append("circle")
+                .attr("r", 4.5)
+                .attr("cx", padding)
+                .attr("cy", padding);
 
-	      // Add a label.
-	      marker.append("text")
-	          .attr("x", padding + 7)
-	          .attr("y", padding)
-	          .attr("dy", ".31em")
-	          .text(function(d) { return d.key; });
+            // Add a label.
+            marker.append("text")
+                .attr("x", padding + 7)
+                .attr("y", padding)
+                .attr("dy", ".31em")
+                .text(function (d) {
+                    return d.key;
+                });
 
-	      function transform(d) {
-	        d = new google.maps.LatLng(d.value[1], d.value[0]);
-	        d = projection.fromLatLngToDivPixel(d);
-	        return d3.select(this)
-	            .style("left", (d.x - padding) + "px")
-	            .style("top", (d.y - padding) + "px");
-	      }
-	    };
-	  };
+            function transform(d) {
+                d = new google.maps.LatLng(d.value[1], d.value[0]);
+                d = projection.fromLatLngToDivPixel(d);
+                return d3.select(this)
+                    .style("left", (d.x - padding) + "px")
+                    .style("top", (d.y - padding) + "px");
+            }
+        };
+    };
 
-	  // d3.select("#slider-time")
-   //    .on("mousemove", function() { 
-   //      update(parseInt(this.value), 500);
-   //      parseInt(this.value)
-   //    })
-
-
+    // d3.select("#slider-time")
+    //    .on("mousemove", function() { 
+    //      update(parseInt(this.value), 500);
+    //      parseInt(this.value)
+    //    })
 
 
 
-	  // Bind our overlay to the map…
-	  overlay.setMap(map);
+
+
+    // Bind our overlay to the map…
+    overlay.setMap(map);
 }
--- public/js/fortuneMap.js
+++ public/js/fortuneMap.js
@@ -1,19 +1,20 @@
 (() => {
     const Aladdin = new blk.API();
-    
+
     'use strict';
     const map = initMap('overlay');
-    
+
     // render an svg as mega container
     const svg = d3.select('#overlay').append('svg')
         .attr('width', '100%')
         .attr('height', '100%');
-    
+
     // colour scale
     const colourScale = d3.scaleSequential(d3.interpolateYlGnBu);
-    
+
     // fetch data and render circle svg elements
     $.get('/data', (res) => {
+        console.log(res);
         d3.json(res, (err, data) => {
             if (err) {
                 console.log(err);
@@ -23,22 +24,22 @@
                 .enter()
                 .append('g')
                 .attr('class', 'circles');
-            
+
             svg.selectAll('circles').each(function (d, i) {
                 let price = d.price;
                 let company = d.company;
-                
+
                 d3.select(this).append('circle')
                     .attr('r', 30) // add interporlation functions
                     .attr('cx', 50)
                     .attr('cy', 50)
                     .attr('fill', colourScale(price));
-            }); 
-                
+            });
+
         });
     });
-    
-    
-    
-    
+
+
+
+
 })();
